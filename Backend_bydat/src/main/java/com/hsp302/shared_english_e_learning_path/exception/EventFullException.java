53fb9f820c3d21827bf7a9792b864ef536ba0167
xuzzz1 <nguyentandat97pch@gmail.com>
2026-01-31 19:31:45 +0700

    xoa

--- Backend_bydat/src/main/java/com/hsp302/shared_english_e_learning_path/exception/EventFullException.java
+++
@@ -1,7 +0,0 @@
-package com.hsp302.shared_english_e_learning_path.exception;
-
-public class EventFullException extends RuntimeException {
-    public EventFullException(String message) {
-        super(message);
-    }
-}
--- Backend_bydat/src/main/java/com/hsp302/shared_english_e_learning_path/exception/InvalidEventException.java
+++
@@ -1,7 +0,0 @@
-package com.hsp302.shared_english_e_learning_path.exception;
-
-public class InvalidEventException extends RuntimeException {
-    public InvalidEventException(String message) {
-        super(message);
-    }
-}
